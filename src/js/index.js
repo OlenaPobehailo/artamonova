be1c5e82e399c141d08f2ebaab1495b7446f3479
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2025-03-08 16:59:52 +0200

    update cases and algoritm sections, update styles

--- src/js/index.js
+++ src/js/index.js
@@ -30,4 +30,59 @@ document.querySelectorAll(".tab-button").forEach((button) => {
   });
 });
 
+// document.getElementById("openSlider").addEventListener("click", function () {
+//   document.getElementById("sliderOverlay").style.display = "flex";
+// });
+// document.getElementById("closeSlider").addEventListener("click", function () {
+//   document.getElementById("sliderOverlay").style.display = "none";
+// });
+
+// var swiper = new Swiper(".swiper-container", {
+//   loop: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+
+document.querySelectorAll(".openSlider").forEach((button) => {
+  button.addEventListener("click", function () {
+    document.getElementById(this.dataset.slider).style.display = "flex";
+  });
+});
+
+document.querySelectorAll(".close-slider").forEach((button) => {
+  button.addEventListener("click", function () {
+    this.parentElement.style.display = "none";
+  });
+});
+
+new Swiper(".swiper1", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper1 .swiper-button-next",
+    prevEl: ".swiper1 .swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper1 .swiper-pagination",
+    clickable: true,
+  },
+});
+
+new Swiper(".swiper2", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper2 .swiper-button-next",
+    prevEl: ".swiper2 .swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper2 .swiper-pagination",
+    clickable: true,
+  },
+});
+
 console.log("script");
